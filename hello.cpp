a299ecf4089fa2e4d1ec6cb46ee7ff83cb010f65
Neilson-M <91747800+Neilson-M@users.noreply.github.com>
2022-01-07 17:18:52 -0800

    week_1 project
    Uploaded a basic "Hello world!" script for completion of Week 1's project.

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+    cout << "I understand this is just a basic hello world but I don't know enough c++ atm to do anything too fancy." << endl;
+
+    return 0;
+}
