0668507185c69b5df3ed5346b16adebeba14806e
Neilson-M <91747800+Neilson-M@users.noreply.github.com>
2022-02-24 14:08:40 -0800

    Update arbitrary_list_operations.cpp

--- Project_4/arbitrary_list_operations.cpp
+++ Project_4/arbitrary_list_operations.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 
+#include "node.cpp"
+
 using std::cout;
 using std::endl;
 
-int main(*argc **argv){
+Node header = nullptr;
+
+void insert_node(node_value, insert_after){
+    Node *temp_pointer = header;
+    Node *temp_next = nullptr;
+    Node *node_to_add = new Node(int node_value);
+    
+    for (int i = 0; i < insert_after; i++){
+        *temp_pointer = *temp_pointer->next;
+    }
+    
+    *temp_next = *temp_pointer->next;
+    *temp_pointer->next = *node_to_add;
+    *node_to_add->next = *temp_next;
+}
+
+int main(int argc, char **argv){
     return 0;
 }
