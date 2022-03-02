264f23dd6b9def4af4de11cda206ce5159bf0f0b
Neilson-M <91747800+Neilson-M@users.noreply.github.com>
2022-03-02 13:06:36 -0800

    Update binary_search_tree.cpp

--- Project_6/binary_search_tree.cpp
+++ Project_6/binary_search_tree.cpp
@@ -6,11 +6,47 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-void add_node(int node_value){
-	pass
+Node *root = nullptr;
+
+void add_node(int insert_value){
+	Node *temp_pointer = root;
+	Node *new_node = new Node(insert_value, 1, nullptr, nullptr);
+	if(root == nullptr){
+		root = new_node;
+	}
+	else{
+		while(True){
+			if(insert_value < temp_pointer->value){ // Traverse left down bst
+				if(temp_pointer->left != nullptr){
+					temp_pointer = temp_pointer->left;
+				}
+				else{
+					temp_pointer->left = new_node;
+					break;
+				}
+			}
+			else if(insert_value > temp_pointer->value){ // Traverse right down bst
+				if(temp_pointer->right != nullptr){
+					temp_pointer = temp_pointer->right;
+				}
+				else{
+					temp_pointer->right = new_node;
+					break;
+				}
+			}
+			else if(insert_value == temp_pointer->value){ // Stack on current node
+				temp_pointer->stack_count++;
+				break;
+			}
+			else{ // Bad end
+				cout << "You shouldn't be here. The balrog now knows your scent. Run." << endl;
+				break;
+			}
+		}
+	}
 }
 
-int remove_node(int node_value){
+int remove_node(int remove_value){
 	pass
 }
 
@@ -38,15 +74,22 @@ void bfs_dequeue(){
 	pass
 }
 
+/*
 void create_binary_tree(int *root_value_array){
-	for(int i = 0; )
+	int place_holder = 0;
+	for(auto i : root_value_array){
+		if(place_holder == 0){
+			root = new Node{i, 1, }
+		}
+	}
 }
+*/
 
 int main(int argc, char **argv){
 	int esc_val = 1;
-	std::array<int, 9> root_value_array = {10, 6, 5, 3, 9, 17, 13, 15, 20};
+	// std::array<int, 9> root_value_array = {10, 6, 5, 3, 9, 17, 13, 15, 20};
 	
-	create_binary_tree(root_value_array)
+	// create_binary_tree(root_value_array)
 	
 	while(esc_val){
 		cout << "What would you like to do? [1]:Add a node. [2]:Remove a node and return its value. [3]:Exit." << endl;
