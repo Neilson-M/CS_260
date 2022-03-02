f3f05d289f8e01e3b796c51aa63524d9db73ebb9
Neilson-M <91747800+Neilson-M@users.noreply.github.com>
2022-03-02 10:51:34 -0800

    Update binary_search_tree.cpp

--- Project_6/binary_search_tree.cpp
+++ Project_6/binary_search_tree.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <array>
 #include "binary_node.cpp"
 
 using std::cin;
@@ -38,12 +39,14 @@ void bfs_dequeue(){
 }
 
 void create_binary_tree(int *root_value_array){
-	
+	for(int i = 0; )
 }
 
 int main(int argc, char **argv){
 	int esc_val = 1;
-	int root_value_array = {10, 6, 5, 3, 9, 17, 13, 15, 20}
+	std::array<int, 9> root_value_array = {10, 6, 5, 3, 9, 17, 13, 15, 20};
+	
+	create_binary_tree(root_value_array)
 	
 	while(esc_val){
 		cout << "What would you like to do? [1]:Add a node. [2]:Remove a node and return its value. [3]:Exit." << endl;
