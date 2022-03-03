cf571adb557f93c8fd8cf118ef17a86cf8496d14
Neilson-M <91747800+Neilson-M@users.noreply.github.com>
2022-03-03 06:42:15 -0800

    Update binary_node.cpp

--- Project_6/binary_node.cpp
+++ Project_6/binary_node.cpp
@@ -8,4 +8,15 @@ struct Node{
     int stack_count;
     Node *left;
     Node *right;
+    
+    Node(int value, int stack_count, Node *left, Node *right){
+        this->value = value;
+        this->stack_count = stack_count;
+        this->left = left;
+        this->right = right;
+    }
+
+    // Node(int value) {
+    //     Node(value, 1, nullptr, nullptr);
+    // }
 };
