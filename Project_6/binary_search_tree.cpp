052f7ebbdc803f465662db1d5c44ed59e66d4d41
Neilson-M <91747800+Neilson-M@users.noreply.github.com>
2022-03-03 15:19:41 -0800

    Update binary_search_tree.cpp

--- Project_6/binary_search_tree.cpp
+++ Project_6/binary_search_tree.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
-#include <array>
+// #include <array>
+#include <string>
 #include "binary_node.cpp"
 
+using std::string;
 using std::cin;
 using std::cout;
 using std::endl;
@@ -75,11 +77,29 @@ void bfs_dequeue(){
 }
 
 void create_binary_tree(int *root_value_array){
-	int place_holder = 0;
-	for(auto i : root_value_array){
-		if(place_holder == 0){
-			root = new Node{i, 1, }
-		}
+	// for(auto i : root_value_array){
+	for(int i = 0; i < 9; i++){
+		add_node(root_value_array[i]);
+	}
+	Node *temp_pointer = root;
+	string print_string;
+	print_current_tree(temp_pointer, print_string);
+}
+
+void print_current_tree(Node *temp_pointer, string print_string){ // Pre-order search w/o search
+	/*
+	1. Access Node
+	2. Recursively travel Left
+	3. Recursively travel Right
+	*/
+	if(!print_string.empty()){
+		print_string = print_string + ", " + temp_pointer->value;
+	}
+	else{
+		print_string = temp_pointer->value;
+	}
+	if(temp_pointer->left != nullptr){
+		
 	}
 }
 
@@ -91,9 +111,10 @@ int main(int argc, char **argv){
 	int esc_val = 1;
     int menu_select;
     int node_value;
-	// std::array<int, 9> root_value_array = {10, 6, 5, 3, 9, 17, 13, 15, 20};
 	
-	// create_binary_tree(root_value_array)
+	// std::array<int, 9> root_value_array = {10, 6, 5, 3, 9, 17, 13, 15, 20}; // I tried
+	int root_value_array[9] = {10, 6, 5, 3, 9, 17, 13, 15, 20};
+	create_binary_tree(root_value_array);
 	
 	while(esc_val){
 		cout << "What would you like to do? [1]:Add a node. [2]:Remove a node and return its value. [3]:Exit." << endl;
