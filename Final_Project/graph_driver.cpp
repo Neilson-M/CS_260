714970fe646ed0ad8dffbbdec0cc12bbb7b6b3e2
Neilson-M <91747800+Neilson-M@users.noreply.github.com>
2022-03-10 15:24:30 -0800

    Update graph_driver.cpp

--- Final_Project/graph_driver.cpp
+++ Final_Project/graph_driver.cpp
@@ -39,5 +39,25 @@ int main(int argc, char **argv){
     return 0;
 }
 
+/*
+use dijkstra's algorithm to determine shortest path
+function Dijkstra(Graph, source):
+    for each vertex v in Graph.Vertices:
+        dist[v] ← INFINITY
+        prev[v] ← UNDEFINED
+        add v to Q
+    dist[source] ← 0
+
+    while Q is not empty:
+        u ← vertex in Q with min dist[u]
+
+        for each neighbor v of u still in Q:
+            alt ← dist[u] + Graph.Edges(u, v)
+            if alt < dist[v]:
+                dist[v] ← alt
+                prev[v] ← u
+    return dist[], prev[]
+*/
+
 // use: g++ -o main.exe .\graph_driver.cpp graph.cpp
 // then use: ./main.exe
